25606aacbca538dba9c0fafa869ffdbcac0f8202
azukauteh <aazure263@gmail.com>
2024-02-08 17:54:33 +0000

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,11 @@ def hash_password(password: str) -> bytes:
     Hashes the password using bcrypt.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password, password):
+    """
+    Validates the provided password against the hashed password using bcrypt.
+    """
+    """ Check if the provided password matches the hashed password"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
